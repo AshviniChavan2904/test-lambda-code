046dad98cb820482341b176e7d6c469aa672fcc6
Ashvini Chavan <AChavan@TechStyle.com>
2024-05-31 18:24:51 +0530

    Added new lambda code

--- index.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event) => {
-    console.log("Event: ", event);
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello, World!'),
-    };
-    return response;
-};
